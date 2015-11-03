e72126cb1dc74ccad47fc89bc7f000964889d44f
thoughtspile <v.klepov@gmail.com>
2015-11-03 16:20:16 +0300

    alpha click

--- process.js
+++ process.js
@@ -1,6 +1,6 @@
 var io = require('fs')
 
-var walk2 = 2;
+var walk2 = 3;
 var walk = Math.sqrt(walk2);
 var reach = function(c1, c2) {
     var dx = Math.abs(c1.x - c2.x);
@@ -12,24 +12,62 @@ var reach = function(c1, c2) {
     return dx * dx + dy * dy <= walk2;
 };
 
+function mean(arr, key) {
+    return arr.reduce(function(a, item) {
+        return a + item[key]; }, 0) / arr.length;
+}
+
+function union(arr, into) {
+    return arr.reduce(function(acc, el) {
+        if (acc.indexOf(el) === -1)
+            acc.push(el);
+        return acc;
+    }, into);
+}
+
 io.readFile(__dirname + '/ground.json', function(err, str) {
-    var lines = {};
     var data = JSON.parse(str).map(function(item) {
         var item = {
             x: item['Cells']['Longitude_WGS84'],
             y: item['Cells']['Latitude_WGS84'],
             lines: item['Cells']['RouteNumbers'].replace(' ', '').split(';')
         };
+        item.x = (item.x - 37.3) * 1000;
+        item.y = (item.y - 55.5) * 1300;
+        return item;
+    });
 
+    data = data.reduce(function(clusters, pt) {
+        var close = clusters.filter(function(cluster) {
+            return reach(pt, cluster);
+        });
+        if (close.length == 0) {
+            clusters.push(pt);
+            return clusters;
+        }
+        close.forEach(function(clust) {
+            clusters.splice(clusters.indexOf(clust), 1);
+        }, []);
+        close.push(pt);
+        var aggregate = {
+            x: mean(close, 'x'),
+            y: mean(close, 'y'),
+            lines: close.reduce(function(acc, item) {
+                return union(item.lines, acc);
+            }, [])
+        };
+        clusters.push(aggregate);
+        return clusters;
+    }, []);
+    console.log(data.length, 'aggregated');
+
+    var lines = {};
+    data.forEach(function(item) {
         item.lines.forEach(function(lineI) {
             lines[lineI] = (lines[lineI] || 0) + 1;
         });
 
         item.lineCount = item.lines.length;
-        item.x = (item.x - 37.3) * 1000;
-        item.y = (item.y - 55.5) * 1300;
-
-        return item;
     });
 
     var lineTiers = {};
@@ -38,16 +76,15 @@ io.readFile(__dirname + '/ground.json', function(err, str) {
             return acc + lines[lineI];
         }, 0);
         item.lines.forEach(function(lineI) {
-            lineTiers[lineI] = (lineTiers[lineI] || 0) + item.tier1;
+            lineTiers[lineI] = union(item.lines, lineTiers[lineI] || []);
         });
-        //data.reduce(function(acc, item2) {
-        //    return acc + reach(item, item2)? item2.lineCount: 0;
-        //}, 0);
     });
 
     data.forEach(function(item) {
         item.tier2 = item.lines.reduce(function(acc, lineI) {
-            return acc + lineTiers[lineI];
+            return acc + lineTiers[lineI].reduce(
+                function(acc, lineI) { return acc + lines[lineI]; },
+                0);
         }, 0);
     })
 
--- vis.js
+++ vis.js
@@ -1,28 +1,57 @@
 (function() {
+    //stops = stops.slice(0, 400);
     var w = 600,
         h = 600;
+
+    var x = d3.scale.linear().domain([0, w]).range([0, w]);
+    var y = d3.scale.linear().domain([0, h]).range([h, 0]);
+
+    var zoom = d3.behavior.zoom()
+        .scaleExtent([1, 20])
+        .x(x).y(y)
+        .on('zoom', fzoom);
+
+    var drag = d3.behavior.drag()
+        .origin(function(d) { return d; })
+        .on("drag", dragged);
+
     var svg = d3.select("body").append("svg")
         .attr("width", w)
         .attr("height", h)
-        .selectAll('circle');
+        .call(zoom)
+        .call(drag);
 
-    var stopData = stops;
-    stops = svg.data(stopData).enter().append('g');
-    stops;
-    stops.append('circle')
-        .attr('fill', 'rgba(0,0,0,.05)')
-        .attr('r', function(pt) { return pt.tier2 / 10000; })
+    var visible = stops.slice();
+    var map = svg.selectAll('circle').data(visible).enter().append('g');
+    map.append('circle')
+        .attr('opacity', '.05')
+        .attr('r', function(pt) { return pt.tier2 / 1000; })
         .attr('cx', function(pt) { return pt.x; })
         .attr('cy', function(pt) { return h - pt.y; });
-    stops.append('circle')
-        .attr('fill', 'rgba(0,0,0,.5)')
+    map.append('circle')
+        .attr('opacity', '.5')
         .attr('r', function(pt) { return pt.tier1 / 100; })
         .attr('cx', function(pt) { return pt.x; })
         .attr('cy', function(pt) { return h - pt.y; })
-        .on('mouseover', function(c) {
-            d3.select(this).attr('fill', 'red');
-        })
-        .on('mouseleave', function(c) {
-            d3.select(this).attr('fill', 'rgba(0,0,0,.05)');
+        .on('click', function(c, pt) {
         });
+    map.on('click', function(item, c) {
+        d3.select(this)
+            .append('text')
+            .attr('x', x(item.x))
+            .attr('y', y(item.y))
+            .attr('font-family', 'sans-serif')
+            .attr('font-size', '8')
+            .text(item.lines.join(', '));
+    })
+
+    function fzoom() {
+        map.attr("transform", transform);
+    }
+    function dragged(d) {
+        svg.attr('transform', transform(d3.event));
+    }
+    function transform(d) {
+        return "translate(" + x(d.x) + "," + y(d.y) + ")";
+    }
 }());
